f288e7f44b9c1cea27fa3c013721368fa10247fb
mini-hawser <ziadnopy2020@gmail.com>
2025-12-05 22:45:59 +0200

    User class

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+      User u = User.getUser(130);
+      u.manageProfile(null,null,0);
+
+    }
+}
--- User.java
+++ User.java
@@ -1,4 +1,6 @@
-public abstract class User {
+import java.io.*;
+import java.util.*;
+public class User {
     private String username;
     private int age;
     private int id;
@@ -6,6 +8,10 @@ public abstract class User {
     private String phoneNumber;
     private String password;
     private String role;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
     public User(String username, int age, int id, String email, String phoneNumber, String password, String role) {
         this.username = username;
@@ -80,19 +86,110 @@ public abstract class User {
         this.role = role;
     }
 
-    public User getUser(){
-        return this;
+    public static User getUser(int id){
+        try (BufferedReader br = new BufferedReader(new FileReader("Accounts.CSV"))) {
+            String line;
+            br.readLine(); // skip header
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                if(id==(Integer.parseInt(data[2]))){
+                    User user = new User(data[0],Integer.parseInt(data[1]),id,data[3],data[4],data[5],data[6]);
+                    return user;
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public void logout(){
 
+    public static boolean login(String Username,String password){
+        try (BufferedReader br = new BufferedReader(new FileReader("Accounts.CSV"))) {
+            String line;
+            br.readLine(); // skip header
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                if(Username.equalsIgnoreCase(data[0])&& password.equals(data[5])){
+                    return true;
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         return false;
     }
 
-    public static void login(String Username,String password){
+    public void manageProfile(String Username,String email,int op){
 
-    }
+        List<User> users = new ArrayList<>();
 
-    public abstract void manageProfile();
+        try (BufferedReader br = new BufferedReader(new FileReader("Accounts.CSV"))) {
+            String line;
+
+            // Skip header
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                User u = new User();
+                u.setUsername(data[0]);
+                System.out.println(data[0]);
+                u.setAge(Integer.parseInt(data[1]));
+                u.setId(Integer.parseInt(data[2]));
+                u.setEmail(data[3]);
+                u.setPhoneNumber(data[4]);
+                u.setPassword(data[5]);
+                u.setRole(data[6]);
+                users.add(u);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(op==0){
+            for(int i=0;i<users.size();i++)
+            if(users.get(i).getId()==this.id){
+                users.remove(i);
+                break;
+            }
+        }
+        else{
+            for(int i=0;i<users.size();i++)
+                if(users.get(i).getId()==this.id){
+                    users.get(i).setUsername(Username);
+                    users.get(i).setEmail(email);
+                    break;
+                }
+        }
+        try (FileWriter writer = new FileWriter("Accounts.CSV")) {
+
+            writer.write("username,age,id,email,phoneNumber,password,role\n");
+
+            for (User u : users) {
+                writer.write(
+                        u.getUsername() + "," +
+                                u.getAge() + "," +
+                               u.getId()+ "," +
+                                u.getEmail() + "," +
+                                u.getPhoneNumber() + "," +
+                                u.getPassword()+","+
+                                u.getRole()+ "\n"
+                );
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    };
 
 
 }
