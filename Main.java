ad83f936abe410c94b453b43659e81b915ce3303
Kareem4805Adel <107364117+Kareem4805Adel@users.noreply.github.com>
2025-12-07 01:09:18 +0200

    addedClasses

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
+        Ebook b = new Ebook(101,"Fiction",LocalDate.of (2020,5,14),124,"A mystery novel set in Victorian London","The Silent Streets","Kareem");
     }
 }
