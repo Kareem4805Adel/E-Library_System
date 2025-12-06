c18b774be1de834222044af2395c6fa0e98369c8
PC <Youssef.Ayman1052@gmail.com>
2025-12-06 23:10:54 +0200

    ba7ebak

--- Ebook.java
+++ Ebook.java
@@ -1,4 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 //hi
 public class Ebook {
     private int BookId;
@@ -78,8 +85,103 @@ public class Ebook {
         Author = author;
     }
 
-    public void removeBook(int bookId){}
-    public void updateBook(int bookId){}
+    public void removeBook(int bookId) {
+        List<Ebook> books = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("Book.CSV"))) {
+            String line;
+            br.readLine(); // skip header
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                Ebook u = new Ebook();
+                u.setBookId(Integer.parseInt(data[0]));
+                System.out.println(data[0]);
+                u.setGenre(data[1]);
+                u.setPublisherId(Integer.parseInt(data[2]));
+                u.setDescription(data[3]);
+                u.setTitle(data[4]);
+                u.setAuthor(data[5]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getBookId() == this.BookId) {
+                books.remove(i);
+                break;
+            }
+
+        }
+        try (FileWriter writer = new FileWriter("Accounts.CSV")) {
+
+            writer.write("BookId,genre,PublishingDate,PublisherId,description,Title,Author\n");
+
+            for (Ebook u : books) {
+                writer.write(
+                        u.getBookId() + "," +
+                                u.getGenre() + "," +
+                                u.getPublishingDate()+ "," +
+                                u.getPublisherId() + "," +
+                                u.getDescription() + "," +
+                                u.getTitle()+","+
+                                u.getAuthor()+ "\n"
+                );
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void updateBook(int bookId,String genre,String description){
+        List<Ebook> books = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("Book.CSV"))) {
+            String line;
+            br.readLine(); // skip header
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                Ebook u = new Ebook();
+                u.setBookId(Integer.parseInt(data[0]));
+                System.out.println(data[0]);
+                u.setGenre(data[1]);
+                u.setPublisherId(Integer.parseInt(data[2]));
+                u.setDescription(data[3]);
+                u.setTitle(data[4]);
+                u.setAuthor(data[5]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getBookId() == this.BookId) {
+                books.get(i).setGenre(genre);
+                books.get(i).setDescription(description);
+
+                break;
+            }
+
+        }
+        try (FileWriter writer = new FileWriter("Accounts.CSV")) {
+
+            writer.write("BookId,genre,PublishingDate,PublisherId,description,Title,Author\n");
+
+            for (Ebook u : books) {
+                writer.write(
+                        u.getBookId() + "," +
+                                u.getGenre() + "," +
+                                u.getPublishingDate()+ "," +
+                                u.getPublisherId() + "," +
+                                u.getDescription() + "," +
+                                u.getTitle()+","+
+                                u.getAuthor()+ "\n"
+                );
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
     public Ebook getBook(int bookId){return this;}
     public void LogVersionHistory(int bookId){}
     public void CreateBook(){}
